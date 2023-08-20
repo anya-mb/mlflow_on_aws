24bdcfba88cbb5734b8d30b932aebe23701234a0
anya-mb <anyamb@yandex.ru>
2023-08-20 00:08:59 -0700

    setup s3 and validations

--- mlflow_on_aws/mlflow_on_aws_stack.py
+++ mlflow_on_aws/mlflow_on_aws_stack.py
@@ -1,19 +1,22 @@
 from aws_cdk import (
     # Duration,
     Stack,
+    aws_s3 as s3,
+    RemovalPolicy,
     # aws_sqs as sqs,
 )
+
 from constructs import Construct
 
-class MlflowOnAwsStack(Stack):
 
+class MlflowOnAwsStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "MlflowOnAwsQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # Create an S3 bucket for MLflow artifacts
+        s3_bucket = s3.Bucket(
+            self,
+            "MLFlowS3Bucket",
+            bucket_name="<YOUR_S3_BUCKET>",
+            removal_policy=RemovalPolicy.DESTROY,
+        )
--- tests/unit/test_mlflow_on_aws_stack.py
+++ tests/unit/test_mlflow_on_aws_stack.py
@@ -10,6 +10,7 @@ def test_sqs_queue_created():
     stack = MlflowOnAwsStack(app, "mlflow-on-aws")
     template = assertions.Template.from_stack(stack)
 
+
 #     template.has_resource_properties("AWS::SQS::Queue", {
 #         "VisibilityTimeout": 300
 #     })
